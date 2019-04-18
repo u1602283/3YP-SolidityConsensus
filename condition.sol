8e58ccb8aa894ebd5db864382bab65d228679f38
Scott P <10318890+u1602283@users.noreply.github.com>
2019-04-18 12:06:42 +0100

    Added comments

--- condition.sol
+++ condition.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.25;
 
+//A fairly self-explanantory class that represents one condition within a contract.
+//At this stage of development, it represents an agreed payment of some amount from one wallet address to another.
 contract Condition{
     bool isNull;
     uint condType;
@@ -9,24 +11,28 @@ contract Condition{
     bool completed;
     
     constructor(uint256 _condType, address _payFrom, address _payTo, uint256 _amount) public {
-        condType = _condType;
+        condType = _condType; //Would be used if further developed to differentiate between Ethereum and real-life transactions
         payFrom = _payFrom;
         payTo = _payTo;
         amount = _amount;
     }
     
+    //Used once everyone accepts a proposal to nullify a condition, prevents execution
     function nullify() public {
         isNull = true;
     }
     
+    //Used during execution
     function complete() public {
         completed = true;
     }
     
+    //Return all of the necessary details about a condition
     function get() view public returns(bool, uint, address, address, uint256, bool){
         return(isNull, condType, payFrom, payTo, amount, completed);
     }
     
+    //Used during debugging
     function getIsNull() view public returns(bool){
         return isNull;
     }
