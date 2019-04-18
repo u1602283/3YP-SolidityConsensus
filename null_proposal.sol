81677b2e60de88c020c7db42e0005c2f7e21d33d
Scott P <10318890+u1602283@users.noreply.github.com>
2019-04-18 12:41:26 +0100

    Added comments

--- null_proposal.sol
+++ null_proposal.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.25;
 
+//A simple class that represents a proposal to nullify a condition.
 contract ProposedNull{
     uint256 idx;
     bool[] consent;
@@ -12,10 +13,13 @@ contract ProposedNull{
         n_parties = _n_parties;
     }
     
+    //Allows a user to agree to a null proposal
     function giveConsent(uint i) public returns(bool){
+        //If this check has been previously completed, skip the check to save gas
         if (allAgreed) {
             return true;
         }
+        //Check to see if consent has been given by all parties
         consent[i] = true;
         for (uint j = 0; j < n_parties; j++){
             if (!(consent[j])){
@@ -26,6 +30,7 @@ contract ProposedNull{
         return allAgreed;
     }
     
+    //Used for debugging purposes
     function get() view public returns(bool[]){
         return consent;
     }
